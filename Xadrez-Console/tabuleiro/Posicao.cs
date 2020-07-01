37ea69ade163ad46bc00ba3c11c7e976f5ac1571
Brenno Baptista <brenno_baptista@hotmail.com>
2020-07-01 11:41:25 -0300

    Implementando o tabuleiro

---
+++ Xadrez-Console/tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿namespace tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            string p = "L: " + Linha +  " C: " + Coluna;
+            return p;
+        }
+    }
+}
